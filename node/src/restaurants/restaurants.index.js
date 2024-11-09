51971f4de82ba9550dff74c55b23e0a2768feaeb
dgatto <11690479+martiangirlie@users.noreply.github.com>
2024-11-09 00:45:03 -0500

    Set up file parsing from CSV to JSON.

--- node/src/restaurants/restaurants.index.js
+++ node/src/restaurants/restaurants.index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const getByDate = require('./restaurants.service');
 const app = (module.exports = express());
 
-app.get('/restaurants/open-by-date/:date', (req, res) => {
+app.get('/restaurants/open-by-date/:date', async (req, res) => {
   const date = req.params.date;
+
+  const results = await getByDate(date);
+
+  res.send(results);
 });
---
+++ node/src/restaurants/restaurants.service.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const parse = require('csv-parser');
+const path = require('path');
+
+/**
+ * @summary Takes in a datetime and returns list of restaurants open on that datetime.
+ * @param date Datetime to check for open restaurants
+ * @returns List of restaurants that meet datetime criteria
+ */
+const getByDate = async (date) => {
+  const filePath = path.resolve(__dirname, './restaurants.csv');
+
+  const results = await parseCSV(filePath);
+
+  console.log(results);
+
+  return results;
+};
+
+/**
+ * @param filePath Filepath of the CSV to be parsed
+ * @summary Takes a file path for a CSV and returns it's parsed output as a Promise
+ * @returns Promise of the filestream parsing the CSV
+ */
+const parseCSV = (filePath) => {
+  return new Promise((resolve, reject) => {
+    let results = [];
+
+    fs.createReadStream(filePath)
+      .pipe(parse())
+      .on('data', (data) => results.push(data))
+      .on('end', () => {
+        resolve(results);
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+};
+
+module.exports = getByDate;
